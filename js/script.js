8123960ed29a29d886cccf70c39a5ce84bfa2bd5
anjahu3 <anja.huber@stud.fhgr.ch>
2026-05-26 15:15:42 +0200

    Timezone
    Timezonen hinzufügen und API Daten abrufen

--- js/script.js
+++ js/script.js
@@ -1,35 +1,40 @@
 
+// Städte mit ihren Koordinaten und Zeitzonen
+
 const cities = {
   "New York": {
     lat: 40.7128,
-    lng: -74.0060
+    lng: -74.0060,
+    timezone: "America/New_York"
   },
-
   Tokio: {
     lat: 35.6764,
-    lng: 139.6503
+    lng: 139.6503,
+    timezone: "Asia/Tokyo"
   },
-
   Kapstadt: {
     lat: -33.9249,
-    lng: 18.4241
+    lng: 18.4241,
+    timezone: "Africa/Johannesburg"
   },
-
   Zürich: {
     lat: 47.3769,
-    lng: 8.5417
+    lng: 8.5417,
+    timezone: "Europe/Zurich"
   },
-
   Reykjavík: {
     lat: 64.1466,
-    lng: -21.9426
+    lng: -21.9426,
+    timezone: "Atlantic/Reykjavik"
   },
-
   Dubai: {
     lat: 25.2048,
-    lng: 55.2708
+    lng: 55.2708,
+    timezone: "Asia/Dubai"
   }
 };
+
+ // Html Elemente holen
 
 const buttons = document.querySelectorAll(".city-buttons button");
 
@@ -38,16 +43,45 @@ const currentTime = document.getElementById("current-time");
 const sunriseTime = document.getElementById("sunrise-time");
 const solarNoonTime = document.getElementById("solar-noon-time");
 const sunsetTime = document.getElementById("sunset-time");
+
 const card = document.querySelector(".city-card");
 const movingSun = document.getElementById("moving-sun");
+const citycontainer = document.getElementById("city-container");
 
+ // Auf Button klicken
 
 buttons.forEach(button => {
-
   button.addEventListener("click", () => {
     const city = button.textContent;
-    console.log(city);
-
+    loadCityData(city);
   });
-
 });
+
+// API abrufen
+
+async function loadCityData(city) {
+  const cityData = cities[city];
+
+  const url = `https://api.sunrise-sunset.org/json?lat=${cityData.lat}&lng=${cityData.lng}&formatted=0`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  const results = data.results;
+  cityName.textContent = city;
+  currentTime.textContent =
+    `Aktuell ist es in ${city} ${formatTime(new Date(), cityData.timezone)} Uhr`;
+
+
+  sunriseTime.textContent = formatTime(results.sunrise, cityData.timezone);
+  solarNoonTime.textContent = formatTime(results.solar_noon, cityData.timezone);
+  sunsetTime.textContent = formatTime(results.sunset, cityData.timezone);
+  citycontainer.style.display = "block";
+}
+
+function formatTime(time, timezone) {
+  return new Date(time).toLocaleTimeString("de-CH", {
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: timezone
+  });
+}
