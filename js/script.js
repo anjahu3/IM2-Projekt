f69162195a7d6a5fbc3639db7dd05b1597e63195
anjahu3 <anja.huber@stud.fhgr.ch>
2026-05-26 13:52:23 +0200

    js
    lat und lng bei jeder Stadt und auf Button klicken dann erscheint auf console

--- js/script.js
+++ js/script.js
@@ -1,1 +1,53 @@
-console.log("blub");
+
+const cities = {
+  "New York": {
+    lat: 40.7128,
+    lng: -74.0060
+  },
+
+  Tokio: {
+    lat: 35.6764,
+    lng: 139.6503
+  },
+
+  Kapstadt: {
+    lat: -33.9249,
+    lng: 18.4241
+  },
+
+  Zürich: {
+    lat: 47.3769,
+    lng: 8.5417
+  },
+
+  Reykjavík: {
+    lat: 64.1466,
+    lng: -21.9426
+  },
+
+  Dubai: {
+    lat: 25.2048,
+    lng: 55.2708
+  }
+};
+
+const buttons = document.querySelectorAll(".city-buttons button");
+
+const cityName = document.getElementById("city-name");
+const currentTime = document.getElementById("current-time");
+const sunriseTime = document.getElementById("sunrise-time");
+const solarNoonTime = document.getElementById("solar-noon-time");
+const sunsetTime = document.getElementById("sunset-time");
+const card = document.querySelector(".city-card");
+const movingSun = document.getElementById("moving-sun");
+
+
+buttons.forEach(button => {
+
+  button.addEventListener("click", () => {
+    const city = button.textContent;
+    console.log(city);
+
+  });
+
+});
